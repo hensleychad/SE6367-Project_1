e9af286131ce2a7f12aafe65202fb91932c8633f
hensleychad <chadhensley@gmail.com>
2018-10-04 16:55:15 -0400

    add functions for tests

--- bin/Hensley-Test-Execution.sh
+++ bin/Hensley-Test-Execution.sh
@@ -1,91 +1,85 @@
-#!/bin/sh
-
-
-# spell --help
-# Usage: spell [OPTION]... [FILE]...
-# This is GNU Spell, a Unix spell emulator.
-
-#   -I, --ispell-version                        Print Ispell's version.
-#   -V, --version                               Print the version number.
-#   -b, --british                               Use the British dictionary.
-#   -d, --dictionary=FILE                       Use FILE to look up words.
-#   -h, --help                          Print a summary of the options.
-#   -i, --ispell=PROGRAM                        Calls PROGRAM as Ispell.
-#   -l, --all-chains                    Ignored; for compatibility.
-#   -n, --number                                Print line numbers before lines.
-#   -o, --print-file-name                       Print file names before lines.
-#   -D, --ispell-dictionary=DICTIONARY  Use DICTIONARY to look up words.
-#   -s, --stop-list=FILE                        Ignored; for compatibility.
-#   -v, --verbose                               Print words not literally found.
-#   -x, --print-stems                   Ignored; for compatibility.
-
-# Please use Info to read more (type `info spell').
+#!/bin/bash
 
 
 ### GLOBALS
 WORKING_DIR=$1
-#SPELL_CMD="/usr/bin/spell"
 SPELL_CMD=$2
 INPUT_DIR="${WORKING_DIR}/input"
+DICT_DIR="${WORKING_DIR}/dictionary"
 
 ### Not sure this will be used
 LOG_DIR="${WORKING_DIR}/log"
 OUTPUT_FILE="${LOG_DIR}/Hensley-Test.Output"
 ERROR_FILE="${LOG_DIR}/Hensley-Test.Error"
+TEST_ARRAY=(standard_test help_test ispell_version)
+
+  logit() 
+  {
+    echo "[`date`] - ${*}" >> ${OUTPUT_FILE}
+    #echo "[${USER}][`date`] - ${*}" >> ${OUTPUT_FILE}
+  }
+
+  add_buffer() {
+    echo "" >> ${OUTPUT_FILE}
+    echo "" >> ${OUTPUT_FILE}
+    echo "" >> ${OUTPUT_FILE}
+    echo "" >> ${OUTPUT_FILE}
+  }
 
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    echo "Usage: $0 <Working Directory> <Shell Command Location e.g. /usr/bin/shell>"
-    exit 1
-fi
+  standard_test() {
+    logit "############################################"
+    logit "###### Start input file testing #####"
 
-echo "###### Start input file testing #####
+    logit "############################################"
+    logit "Clean test - Test sample file against spell"
+    logit "############################################"
+    logit "Executing ${SPELL_CMD} ${INPUT_DIR}/sample"
+    cmd=`${SPELL_CMD} ${INPUT_DIR}/sample`
+    logit "$cmd"
+    logit "###### End input file testing #####"
+    add_buffer
+  }
 
-echo "############################################"
-echo "Clean test - Test sample file against spell"
-echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/sample
+  help_test() {
+    logit "############################################"
+    logit "###### Start Show help test case #####"
+    logit "############################################"
+    logit "Help test - Show help"
+    logit "   -h, --help                          Print a summary of the options."
+    logit "############################################"
+    cmd=`${SPELL_CMD} --help`
+    logit "$cmd"
+    logit "###### End Show help test case #####"
+  }
 
-echo "############################################"
-echo "Destructive test - File empty"
-echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/empty_sample
+  ispell_version() {
+    logit "############################################"
+    logit "###### Start print ispell version #####"
+    logit "############################################"
+    logit "   -I, --ispell-version                        Print Ispell's version."
+    logit "############################################"
+    cmd=`${SPELL_CMD} ${INPUT_DIR}/sample -I`
+    logit "$cmd"
+    logit "###### End print ispell version #####"
+  }
 
-echo "############################################"
-echo "Destructive test - File does not exist"
-echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/sample_none
+  for test_method in "${TEST_ARRAY[@]}"
+  do
+    echo "${test_method}"
+    ${test_method}
+  done
 
-echo "###### End input file testing #####"
+exit 0
 
-
-echo "###### Start  #####"
-echo "############################################"
-echo "Help test - Show help"
-#   -h, --help                          Print a summary of the options.
-echo "############################################"
-${SPELL_CMD} -h
-${SPELL_CMD} --help
-echo "###### End  #####"
-
-
-echo "###### Start  #####"
-echo "############################################"
-echo "   -I, --ispell-version                        Print Ispell's version."
-echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/sample -I
-${SPELL_CMD} ${INPUT_DIR}/sample --spell-version
-echo "###### End  #####"
-
-echo "###### Start  #####"
-echo "############################################"
-echo "   -V, --version                               Print the version number."
-echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/sample -V
+  logit "###### Start print version number  #####"
+  logit "############################################"
+  logit "   -V, --version                               Print the version number."
+  logit "############################################"
 ${SPELL_CMD} ${INPUT_DIR}/sample --version
-echo "###### End  #####"
+  logit "###### End print version number #####"
+  logit "\n\n\n\n"
+
 
 echo "###### Start  #####"
 echo "############################################"
@@ -94,13 +88,16 @@ echo "############################################"
 ${SPELL_CMD} ${INPUT_DIR}/sample -b
 ${SPELL_CMD} ${INPUT_DIR}/sample --british
 echo "###### End  #####"
+echo ""
+echo ""
+echo ""
+echo ""
 
 echo "###### Start  #####"
 echo "############################################"
 echo "#   -d, --dictionary=FILE                       Use FILE to look up words."
 echo "############################################"
-${SPELL_CMD} ${INPUT_DIR}/sample -d
-${SPELL_CMD} ${INPUT_DIR}/sample --dictionary
+${SPELL_CMD} ${INPUT_DIR}/sample --dictionary=${DICT_DIR}/british-english
 echo "###### End  #####"
 
 echo "###### Start  #####"
@@ -169,8 +166,30 @@ echo "###### End  #####"
 
 echo "###### Start  #####"
 echo "############################################"
-echo "#   -q 	unsupported flag
+echo "#   -q 	unsupported flag"
 echo "############################################"
 ${SPELL_CMD} ${INPUT_DIR}/sample -q
 echo "###### End  #####"
 
+echo "###### Start  #####"
+echo "############################################"
+echo "Destructive test - File empty"
+echo "############################################"
+${SPELL_CMD} ${INPUT_DIR}/empty_sample
+
+echo "############################################"
+echo "Destructive test - File does not exist"
+echo "############################################"
+${SPELL_CMD} ${INPUT_DIR}/sample_none
+echo "###### End  #####"
+}
+
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Usage: $0 <Working Directory> <Shell Command Location e.g. /usr/bin/shell>"
+    exit 1
+fi
+
+runTests
