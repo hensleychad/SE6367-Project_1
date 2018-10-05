61ed9e9e49588bc7cfbda0f226cabb9136ee934e
hensleychad <chadhensley@gmail.com>
2018-10-05 08:52:34 -0400

    fixed logging

--- bin/Hensley-Test-Execution.sh
+++ bin/Hensley-Test-Execution.sh
@@ -10,39 +10,124 @@ DICT_DIR="${WORKING_DIR}/dictionary"
 LOG_DIR="${WORKING_DIR}/log"
 OUTPUT_FILE="${LOG_DIR}/Hensley-Test.Output"
 ERROR_FILE="${LOG_DIR}/Hensley-Test.Error"
-TEST_ARRAY=(standard_test help_test ispell_version spell_version other_dictionary ispell_program all_chains print_file_name print_numbers_on_lines print_words_not_literally_found print_stems test_empty_file  test_upsupported_flag)
+#TEST_ARRAY=(standard_test help_test ispell_version spell_version other_dictionary ispell_program all_chains print_file_name print_numbers_on_lines print_words_not_literally_found print_stems test_empty_file  test_upsupported_flag)
+TEST_ARRAY=(other_dictionary)
 ERROR_MSG="Check Error Log, ${ERROR_FILE}"
 
+WHICH_CMD="/usr/bin/which"
+TOUCH_CMD=`${WHICH_CMD} touch`
+RM_CMD="${WHICH_CMD} rm"
+ECHO_CMD="${WHICH_CMD} echo"
+
+
+
+###############  START Pre-Flight checks  ###################3
+  touch_error_file() {
+   if [ ! -e ${ERROR_FILE} ]; then
+    touch ${ERROR_FILE}
+    ${ECHO_CMD} "Creating error log, if not you miss first timestamp in error file"
+   fi
+  }
+
+  check_input_dir_exist() {
+   if [ ! -d ${INPUT_DIR} ]; then
+    echo "${INPUT_DIR}: not found"
+    exit 1
+   else
+    ${ECHO_COMMAND} "Input directory is  ${INPUT_DIR}"
+   fi
+  }
+
+  check_wrking_dir_exist() {
+   if [ ! -d ${WORKING_DIR} ]; then
+    echo "${WORKING_DIR}: not found"
+    usage
+    exit 1
+   else
+    echo "Working directory is  ${WORKING_DIR}"
+   fi
+  }
+
+  check_dict_dir_exist() {
+   if [ ! -d ${DICT_DIR} ]; then
+    echo "${DICT_DIR}: not found"
+    exit 1
+   else
+    echo "Dictionary files can be found at ${DICT_DIR}"
+   fi
+  }
+
+  check_log_dir_exist() {
+   if [ ! -d ${LOG_DIR} ]; then
+    echo "${LOG_DIR}: not found"
+    exit 1
+   else
+    echo "Output and error logs can be found at ${LOG_DIR}"
+   fi
+  }
+
+  spell_cmd_check() {
+   if [ ! -f ${SPELL_CMD} ]; then
+    echo ":${SPELL_CMD}: not found"
+    usage
+    exit 1
+   else
+    echo "Using spell command: ${SPELL_CMD}"
+   fi
+  }
+
+###############  END  Pre-Flight checks  ###################3
+
+###############  START  Utils  ###################3
+
+  usage() {
+    echo "Usage: $0 <Working Directory> <Shell Command Location e.g. /usr/bin/shell>"
+  }
 
   log_it() {
     echo -e "[`date`] - ${*}" >> ${OUTPUT_FILE}
   }
 
+  mark_it() {
+    echo -e "[`date`] - ${*}" >> ${ERROR_FILE}
+  }
+
   clean_logs() {
    if [ -f $OUTPUT_FILE ]; then
      echo "Cleaning up log file ${OUTPUT_FILE}"
-     rm ${OUTPUT_FILE}
+     ${RM_CMD} ${OUTPUT_FILE}
    fi
   }
 
   clean_error_logs() {
    if [ -f $ERROR_FILE ]; then
      echo "Cleaning up error file ${ERROR_FILE}"
-     rm ${ERROR_FILE}
+     ${RM_CMD} ${ERROR_FILE}
    fi
   }
-
 
   add_buffer() {
     echo -e "[`date`] \n++\n++\n++\n++" >> ${OUTPUT_FILE}
   }
 
+  pre_flight_check() {
+    check_wrking_dir_exist
+    spell_cmd_check
+    check_input_dir_exist
+    check_dict_dir_exist
+    check_log_dir_exist
+    clean_logs
+    clean_error_logs
+    touch_error_file
+  }
 
+###############  END  Utils  ###################3
+
+###############  START  Tests  ###################3
   standard_test() {
     cmd_string="${SPELL_CMD} ${INPUT_DIR}/sample 2> ${ERROR_FILE}"
-    cmd="${ERROR_MSG}"
-    log_it "-- Start input file testing #####"
     log_it "############################################"
+    log_it "-- Start input file testing #####"
     log_it "Clean test - Test sample file against spell"
     log_it "Exeucting the following command: ${cmd_string}"
     cmd=`${SPELL_CMD} ${INPUT_DIR}/sample 2> ${ERROR_FILE}`
@@ -54,11 +139,11 @@ ERROR_MSG="Check Error Log, ${ERROR_FILE}"
 
   help_test() {
     cmd_string="${SPELL_CMD} --help 2> ${ERROR_FILE}"
-    cmd="${ERROR_MSG}"
-    log_it "-- Start Show help test case #####"
     log_it "############################################"
+    log_it "-- Start Show help test case #####"
     log_it "Exeucting the following command: ${cmd_string}"
     cmd=`${SPELL_CMD} --help 2> ${ERROR_FILE}`
+    log_it "NOTE: The help flag echos the the usage of spell STDOUT then writes the help menu to STDERR. See ${ERROR_FILE} for help menu output"
     log_it "Output:\n$cmd"
     log_it "-- End Show help test case #####"
     log_it "############################################"
@@ -67,9 +152,8 @@ ERROR_MSG="Check Error Log, ${ERROR_FILE}"
 
   ispell_version() {
     cmd_string="${SPELL_CMD} -I 2> ${ERROR_FILE}"
-    cmd="${ERROR_MSG}"
-    log_it "-- Start print ispell version #####"
     log_it "############################################"
+    log_it "-- Start print ispell version #####"
     log_it "Exeucting the following command: ${cmd_string}"
     cmd=`${SPELL_CMD} -I 2> ${ERROR_FILE}`
     log_it "Output:\n$cmd"
@@ -80,11 +164,11 @@ ERROR_MSG="Check Error Log, ${ERROR_FILE}"
 
   spell_version () {
     cmd_string="${SPELL_CMD} --version 2>${ERROR_FILE}"
-    cmd="${ERROR_MSG}"
-    log_it "-- Start print version number  #####"
     log_it "############################################"
+    log_it "-- Start print version number  #####"
     log_it "Exeucting the following command: ${cmd_string}"
-    cmd=`${cmd_string}`
+    log_it "NOTE: The --version flag echos version output to STDERR and nothing to STDOUT. See ${ERROR_FILE} for version output"
+    cmd=`${SPELL_CMD} --version 2>${ERROR_FILE}`
     log_it "Output:\n$cmd"
     log_it "-- End print version number #####"
     log_it "############################################"
@@ -97,7 +181,10 @@ ERROR_MSG="Check Error Log, ${ERROR_FILE}"
     log_it "-- Start  use other file to look up words #####"
     log_it "############################################"
     log_it "Exeucting the following command: ${cmd_string}"
-    cmd=`${cmd_string}`
+    log_it "NOTE: The --dictionary/-d flag echos to both STDERR and STDOUT. See ${ERROR_FILE} for error output"
+    mark_it "Start ERROR from $cmd_string"
+    cmd=`${SPELL_CMD} ${INPUT_DIR}/sample --dictionary=${DICT_DIR}/british-english 2> ${ERROR_FILE}`
+    mark_it "End ERROR from $cmd_string"
     log_it "Output:\n$cmd"
     log_it "-- End  use other file to look up words #####"
     log_it "############################################"
@@ -209,12 +296,17 @@ ERROR_MSG="Check Error Log, ${ERROR_FILE}"
   }
 
   run_tests() {
+    echo "Test count  ${#TEST_ARRAY[@]}"
+    echo "Results can be found in ${OUTPUT_FILE}"
+    echo "Errors can be found in ${ERROR_FILE}"
     for test_method in "${TEST_ARRAY[@]}"
     do
       log_it "Running ${test_method}"
       ${test_method}
     done
   }
+
+###############  END  Tests  ###################3
 
 if [ $# -eq 0 ]
   then
@@ -223,6 +315,6 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-  clean_logs
-  clean_error_logs
-  run_tests
+  touch_error_file
+  #pre_flight_check
+  #run_tests
